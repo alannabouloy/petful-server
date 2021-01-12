69d0c29d07a0264a89bd0514cee56cde9b0eed4b
Alanna Bouloy <lanihope@gmail.com>
2021-01-12 11:00:22 -0500

    changed cors settings

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN,
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-adoption-roan.vercel.app/adoption',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
 }
 
